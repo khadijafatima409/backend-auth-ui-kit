46deb35207e58910f1370391572d4afe63e9e294
khadjafatima409 <khadijafatima530@gmail.com>
2025-10-30 17:11:49 +0500

    feat: add User model and migrations for input fields

---
+++ app/models/user.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { compose } from '@adonisjs/core/helpers'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import hash from '@adonisjs/core/services/hash'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+  @column()
+  declare first_name: string
+  @column()
+  declare last_name: string
+  @column()
+  declare email: string
+  @column()
+  declare phone_number: string
+  @column()
+  declare password: string
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
+}
